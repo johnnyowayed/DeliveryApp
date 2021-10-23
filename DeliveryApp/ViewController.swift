247bf00b6883cd975f7aa3f0b1cc873027d290af
johnnyowayed-saradarbank <johnny.owayed@saradarbank.com>
2021-10-23 11:51:20 +0300

    Initial Commit

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by Johnny on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
