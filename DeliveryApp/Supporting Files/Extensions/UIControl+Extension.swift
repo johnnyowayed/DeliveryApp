096a1785d53d7c016c220b1d9ece4807c8ce2bf3
johnnyowayed <johnny.owayed@gmail.com>
2021-10-25 23:06:13 +0300

    UI bug fixes

--- DeliveryApp/Supporting Files/Extensions/UIControl+Extension.swift
+++ DeliveryApp/Supporting Files/Extensions/UIControl+Extension.swift
@@ -19,18 +19,3 @@ extension UIControl {
         objc_setAssociatedObject(self, "\(UUID())", sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
     }
 }
-
-extension UIButton {
-    private func actionHandler(action:(() -> Void)? = nil) {
-        struct __ { static var action :(() -> Void)? }
-        if action != nil { __.action = action }
-        else { __.action?() }
-    }
-    @objc private func triggerActionHandler() {
-        self.actionHandler()
-    }
-    func actionHandler(controlEvents control :UIControl.Event, ForAction action:@escaping () -> Void) {
-        self.actionHandler(action: action)
-        self.addTarget(self, action: #selector(triggerActionHandler), for: control)
-    }
-}
